60bc7b3878cd94151a3b17c0176f52f1d7a98a9b
Tumelo Edward Ntsilo <122849519+TumeloNtsilo@users.noreply.github.com>
2025-03-29 18:25:43 +0200

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,97 +1,77 @@
 import random
 
 class GameOptions:
-
     game_options = ["rock", "paper", "scissors"]
 
     def get_game_options(self):
         print("These are your game options:")
-        for index,option in enumerate(self.game_options):
+        for index, option in enumerate(self.game_options):
             print(f"{index}: {option}")
-
         print()
 
-class Play():
-    score = [0, 0]
-
-    def __init__(self) -> None:
+class Play:
+    def __init__(self):
         self.results = []
 
-    def playerturn(self):
+    def player_turn(self):
         while True:
             try:
-                self.player_turn = int(input(f"Use index number from to make a choice {list(enumerate(GameOptions.game_options))}: "))
-                if 0 <= self.player_turn < len(GameOptions.game_options):
+                choice = int(input(f"Use index number to make a choice {list(enumerate(GameOptions.game_options))}: "))
+                if 0 <= choice < len(GameOptions.game_options):
+                    self.player_choice = GameOptions.game_options[choice]
+                    self.results.append(self.player_choice)
                     break
                 print("Invalid choice, try again.")
             except ValueError:
                 print("Invalid input, enter a number.")
 
-
-
-        self.player_choice = GameOptions.game_options[self.player_turn]
-        self.results.append(self.player_choice)
-
-    def computerturn(self):
+    def computer_turn(self):
         self.computer_choice = random.choice(GameOptions.game_options)
         self.results.append(self.computer_choice)
 
     def display_results(self):
-        print(f"round results: {tuple(self.results)}")
-        print()
+        print(f"Round results: {tuple(self.results)}\n")
 
-class Winner():
-
-    def __init__(self) -> None:
+class Winner:
+    def __init__(self):
         self.player_score = 0
         self.comp_score = 0
 
     def score_count(self, results):
-        if results in [("rock", "scissors"), ("paper", "rock"), ("scissors", "paper")]:
+        player, computer = results  # Unpack tuple
+        if (player, computer) in [("rock", "scissors"), ("paper", "rock"), ("scissors", "paper")]:
             print("You win this round.")
             self.player_score += 1
-        
-        elif results[0] == results[1]:
-            print("It's a tie on this round.")
-            self.player_score += 1
-            self.comp_score += 1
-
+        elif player == computer:
+            print("It's a tie this round.")
         else:
             print("You lost this round.")
             self.comp_score += 1
 
-    def dispay_score(self):
-        print(f"Player {self.player_score} : {self.comp_score} Computer")
-        print()
+    def display_score(self):
+        print(f"Score - Player: {self.player_score} | Computer: {self.comp_score}\n")
 
-    
     def display_winner(self):
         if self.player_score > self.comp_score:
-            print("Congratulations you won!!")
-
+            print("Congratulations! You won!!")
         elif self.player_score == self.comp_score:
             print("It's a tie!")
-
         else:
             print("You lose!!")
 
-
-
 if __name__ == "__main__":
-
     options = GameOptions()
     options.get_game_options()
 
-
     winner = Winner()
+    game = Play()
 
-    for game in range(3):
-        game = Play()
-        game.playerturn()
-        game.computerturn()
+    for _ in range(3):
+        game.results = [] 
+        game.player_turn()
+        game.computer_turn()
         game.display_results()
         winner.score_count(game.results)
-        winner.dispay_score()
-    
-    
+        winner.display_score()
+
     winner.display_winner()

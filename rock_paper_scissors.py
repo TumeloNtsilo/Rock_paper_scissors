ac651bf98c2de451c2ebe7239abcd96a74d48995
Tumelo <tumelontsilo@gmail.com>
2025-07-28 20:33:13 +0200

    Modified the main file

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,8 +1,10 @@
 import random
 
+
 class GameOptions:
     game_options = ["rock", "paper", "scissors"]
 
+    """Prints out all the game options."""
     def get_game_options(self):
         print("These are your game options:")
         for index, option in enumerate(self.game_options):
@@ -13,6 +15,8 @@ class Play:
     def __init__(self):
         self.results = []
 
+    """Allows the player to make a choice during the game from the game choices.
+        The player makes this choice by the choice's index in the list"""
     def player_turn(self):
         while True:
             try:
@@ -25,10 +29,13 @@ class Play:
             except ValueError:
                 print("Invalid input, enter a number.")
 
+                
+    """Allows the computer to randomly make it's choice."""
     def computer_turn(self):
         self.computer_choice = random.choice(GameOptions.game_options)
         self.results.append(self.computer_choice)
 
+    """Displays the results after the player and computer make their choices."""
     def display_results(self):
         print(f"Round results: {tuple(self.results)}\n")
 
@@ -37,8 +44,12 @@ class Winner:
         self.player_score = 0
         self.comp_score = 0
 
+    """Checks for the winner for each round, assigns and add the points.
+        A win equals to one point
+        A tie/draw results in no points for both the player and computer
+        """
     def score_count(self, results):
-        player, computer = results  # Unpack tuple
+        player, computer = results
         if (player, computer) in [("rock", "scissors"), ("paper", "rock"), ("scissors", "paper")]:
             print("You win this round.")
             self.player_score += 1
@@ -48,9 +59,11 @@ class Winner:
             print("You lost this round.")
             self.comp_score += 1
 
+    """Displays the final score"""
     def display_score(self):
         print(f"Score - Player: {self.player_score} | Computer: {self.comp_score}\n")
 
+    """Displays the winner after all 3 rounds."""
     def display_winner(self):
         if self.player_score > self.comp_score:
             print("Congratulations! You won!!")
